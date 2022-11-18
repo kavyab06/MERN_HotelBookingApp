880730748ee3e85d379594f9af591cf0d1c0a460
Kavya <72342210+kavyab06@users.noreply.github.com>
2022-11-18 16:25:17 +0530

    Update City.jsx

--- src/components/city/City.jsx
+++ src/components/city/City.jsx
@@ -8,35 +8,35 @@ const City = () => {
         <div className="cityImages">
             <div className="cityImage">
                 <div className="cityImg">
-                    <img src="./assets/images/kg.jpg" alt="" />
+                    <img src="./assets/images/kg.jpg" alt="Kragujevac" />
                 </div>
                 <h4 className='cityImgTitle'>Kragujevac</h4>
                 <span className='cityImgProperties'>320 properties</span>
             </div>
             <div className="cityImage">
                 <div className="cityImg">
-                    <img src="./assets/images/bg.jpg" alt="" />
+                    <img src="./assets/images/bg.jpg" alt="Belgrade" />
                 </div>
                 <h4 className='cityImgTitle'>Belgrade</h4>
                 <span className='cityImgProperties'>2120 properties</span>
             </div>
             <div className="cityImage">
                 <div className="cityImg">
-                    <img src="./assets/images/ac.jpg" alt="" />
+                    <img src="./assets/images/ac.jpg" alt="Aleksandrovac Zupa" />
                 </div>
-                <h4 className='cityImgTitle'>Aleksandrovac</h4>
+                <h4 className='cityImgTitle'>Aleksandrovac Zupa</h4>
                 <span className='cityImgProperties'>4 properties</span>
             </div>
             <div className="cityImage">
                 <div className="cityImg">
-                    <img src="./assets/images/le.jpg" alt="" />
+                    <img src="./assets/images/le.jpg" alt="Leskovac" />
                 </div>
                 <h4 className='cityImgTitle'>Leskovac</h4>
                 <span className='cityImgProperties'>62 properties</span>
             </div>
             <div className="cityImage">
                 <div className="cityImg">
-                    <img src="./assets/images/ns.jpeg" alt="" />
+                    <img src="./assets/images/ns.jpeg" alt="Novi Sad" />
                 </div>
                 <h4 className='cityImgTitle'>Novi Sad</h4>
                 <span className='cityImgProperties'>1340 properties</span>
