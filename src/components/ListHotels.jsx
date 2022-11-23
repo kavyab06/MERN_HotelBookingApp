8945230103d5ef76d97c88862ed0a4812f580c11
Kavya <72342210+kavyab06@users.noreply.github.com>
2022-11-24 00:15:18 +0530

    Create ListHotels.jsx

---
+++ src/components/ListHotels.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ItemHotels from '../itemHotels/ItemHotels'
+import './listhotels.css'
+
+const ListHotels = () => {
+
+    return (
+        <div className="listHotels">
+            <ItemHotels />
+        </div>
+    )
+}
+
+export default ListHotels
