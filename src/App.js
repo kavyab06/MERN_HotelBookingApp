a8d94fed705a6bc66c53cb1b2032f7cbdfc7e1fc
Kavya <72342210+kavyab06@users.noreply.github.com>
2022-11-24 00:27:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './pages/home/Home';
 import Hotel from './pages/Hotel/Hotel';
 import Hotels from './pages/hotels/Hotels';
+import Login from './pages/login/Login';
+
 function App() {
   return (
     <div>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/hotels' element={<Hotels />} />
-          <Route path='/hotels/:id' element={<Hotel />} />
-        </Routes>
-      </Router>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/hotels' element={<Hotels />} />
+            <Route path='/hotels/:id' element={<Hotel />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </Router>
+
     </div>
   );
 }
