e6b4d963a1e3483f7d64a3c24feb8fd7a5966f78
Kavya <72342210+kavyab06@users.noreply.github.com>
2022-11-24 00:10:13 +0530

    Update City.jsx

--- src/components/city/City.jsx
+++ src/components/city/City.jsx
@@ -1,56 +1,67 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import useFetch from '../../hooks/useFetch'
 import './city.css'
+
 const City = () => {
+
+    const {data, loading, error} = useFetch("api/hotels/bycity?cities=Kragujevac,Leskovac,Belgrade,Novi Sad,Aleksandrovac Zupa");
+    //console.log(data);
+
   return (
     <div className='cities'>
-        <h2 className='cityTitle'>Explore Serbia</h2>
+        <h2 className="cityTitle">Explore Serbia</h2>
         <p className='cityDesc'>These popular destinations have a lot to offer</p>
         <div className="cityImages">
-            <div className="cityImage">
-                <Link to='/'>
-                    <div className="cityImg">
-                        <img src="./assets/images/kg.jpg" alt="Kragujevac" />
+            {loading ? (<h2>Loading...</h2>) : (
+                <>
+                    <div className="cityImage">
+                        <Link to='/'>
+                            <div className="cityImg">
+                                <img src="./assets/images/kg.jpg" alt="Kragujevac" />
+                            </div>
+                            <h4 className='cityImgTitle'>Kragujevac</h4>
+                            <span className='cityImgProperties'>{data[0]} properties</span>
+                        </Link>
                     </div>
-                    <h4 className='cityImgTitle'>Kragujevac</h4>
-                    <span className='cityImgProperties'>320 properties</span>
-                </Link>
-            </div>
-            <div className="cityImage">
-                <Link to='/'>
-                    <div className="cityImg">
-                        <img src="./assets/images/bg.jpg" alt="Belgrade" />
+                    <div className="cityImage">
+                        <Link to='/'>
+                            <div className="cityImg">
+                                <img src="./assets/images/bg.jpg" alt="Belgrade" />
+                            </div>
+                            <h4 className='cityImgTitle'>Belgrade</h4>
+                            <span className='cityImgProperties'>{data[2]} properties</span>
+                        </Link>
                     </div>
-                    <h4 className='cityImgTitle'>Belgrade</h4>
-                    <span className='cityImgProperties'>2120 properties</span>
-                </Link>
-            </div>
-            <div className="cityImage">
-                <Link to='/'>
-                    <div className="cityImg">
-                        <img src="./assets/images/ac.jpg" alt="Aleksandrovac Zupa" />
+                    <div className="cityImage">
+                        <Link to='/'>
+                            <div className="cityImg">
+                                <img src="./assets/images/ac.jpg" alt="Aleksandrovac Zupa" />
+                            </div>
+                            <h4 className='cityImgTitle'>Aleksandrovac Zupa</h4>
+                            <span className='cityImgProperties'>{data[4]} properties</span>
+                        </Link>
                     </div>
-                    <h4 className='cityImgTitle'>Aleksandrovac Zupa</h4>
-                    <span className='cityImgProperties'>4 properties</span>
-                </Link>
-            </div>
-            <div className="cityImage">
-                <Link to='/'>
-                    <div className="cityImg">
-                        <img src="./assets/images/le.jpg" alt="Leskovac" />
+                    <div className="cityImage">
+                        <Link to='/'>
+                            <div className="cityImg">
+                                <img src="./assets/images/le.jpg" alt="Leskovac" />
+                            </div>
+                            <h4 className='cityImgTitle'>Leskovac</h4>
+                            <span className='cityImgProperties'>{data[1]} properties</span>
+                        </Link>
                     </div>
-                    <h4 className='cityImgTitle'>Leskovac</h4>
-                    <span className='cityImgProperties'>62 properties</span>
-                </Link>
-            </div>
-            <div className="cityImage">
-                <Link to='/'>
-                    <div className="cityImg">
-                        <img src="./assets/images/ns.jpeg" alt="Novi Sad" />
+                    <div className="cityImage">
+                        <Link to='/'>
+                            <div className="cityImg">
+                                <img src="./assets/images/ns.jpeg" alt="Novi Sad" />
+                            </div>
+                            <h4 className='cityImgTitle'>Novi Sad</h4>
+                            <span className='cityImgProperties'>{data[3]} properties</span>
+                        </Link>
                     </div>
-                    <h4 className='cityImgTitle'>Novi Sad</h4>
-                    <span className='cityImgProperties'>1340 properties</span>
-                </Link>
-            </div>
+                </>
+            )}
         </div>
     </div>
   )
